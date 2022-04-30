2ad2789184c4546ce875f940c1b046bce18257f2
Elaine <elainedevjava@gmail.com>
2022-04-29 22:29:54 -0300

    Primeiro programa usando eclipse

---
+++ DominandoIdesJava/eclipse/teste-eclipse/src/com/dio/TesteEclipse.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class TesteEclipse {
+	
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+	}
+
+	/*public static void main(String[] args) {
+		System.out.println("Hello Word!");	
+	}*/
+
+}
